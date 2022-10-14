7ab017e0fe85b6b37c855af2aea9d1ddbc241386
Ashbel <ashbelr94@gmail.com>
2022-10-14 18:13:21 +0530

    Db updated

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -1,29 +1,7 @@
 CREATE USER DbTest with PASSWORD 'test' CREATEDB;
 
--- SELECT 'CREATE DATABASE "account-serviceDb" WITH OWNER = DbTest
--- ENCODING = ''UTF8''
--- LC_COLLATE = ''en_US.utf8''
--- LC_CTYPE = ''en_US.utf8''
--- TABLESPACE = pg_default
--- CONNECTION LIMIT = -1;' WHERE NOT EXISTS(SELECT FROM pg_database WHERE datname = 'account-serviceDb')\gexec;
---
--- SELECT 'CREATE DATABASE "transaction-serviceDb" WITH OWNER = DbTest
--- ENCODING = ''UTF8''
--- LC_COLLATE = ''en_US.utf8''
--- LC_CTYPE = ''en_US.utf8''
--- TABLESPACE = pg_default
--- CONNECTION LIMIT = -1;' WHERE NOT EXISTS(SELECT FROM pg_database WHERE datname = 'transaction-serviceDb')\gexec;
+SELECT 'CREATE DATABASE "account-serviceDb"' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'account-serviceDb')\gexec
 
-CREATE DATABASE "account-serviceDb" WITH OWNER = DbTest
-ENCODING = 'UTF8'
-LC_COLLATE = 'en_US.utf8'
-LC_CTYPE = 'en_US.utf8'
-TABLESPACE = pg_default
-CONNECTION LIMIT = -1;
+SELECT 'CREATE DATABASE "transaction-serviceDb"' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'transaction-serviceDb')\gexec
 
-CREATE DATABASE "transaction-serviceDb" WITH OWNER = DbTest
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
+SELECT datname  FROM pg_database;
